b1b9e4227e5b6eec8fef966b42b95516495b1eb3
amm414 <amm414@pitt.edu>
2019-11-03 17:27:55 -0500

    fixed full posting

--- demo/models.py
+++ demo/models.py
@@ -19,28 +19,28 @@ def initdb_command():
 
 
 class User(db.Model):
-	user_id   	 	= db.Column(db.Integer,    primary_key = True)
+	userid   	 	= db.Column(db.Integer,    primary_key = True)
 	username  	 	= db.Column(db.String(24), nullable = False)
 	email    	 	= db.Column(db.String(80), nullable = False)
-	password 	 	= db.Column(db.String(64), nullable = False)
+	# hashed password is 128 chars ALWAYS
+	password 	 	= db.Column(db.String(128), nullable = False)
 	phonenumber  	= db.Column(db.String(64), nullable = False)
 	personalemail  	= db.Column(db.String(80))
-	contactmethod   = db.Column(db.Integer, nullable = False)
 	bio		  	 	= db.Column(db.String(140))
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 
 class Posting(db.Model):
-	post_id 		= db.Column(db.Integer, primary_key = True)
-	user_id 		= db.Column(db.Integer, nullable = False)
+	postid 			= db.Column(db.Integer, primary_key = True)
+	userid 			= db.Column(db.Integer, nullable = False)
 	date 			= db.Column(db.Date, nullable = False)
 	title   		= db.Column(db.String(80), nullable = False)
 	description 	= db.Column(db.String(140), nullable = False)
 	price 			= db.Column(db.Integer, nullable = False)
 	category 		= db.Column(db.String(80), nullable = False)
 	contactmethod 	= db.Column(db.String(80), nullable = False)
-	tag 			= db.Column(db.String(80), nullable = False)
-
+	tags 			= db.Column(db.String(80), nullable = False)
+	
 	def __repr__(self):
 		return '<Posting {}: "{}">'.format(self.post_id, self.title)
