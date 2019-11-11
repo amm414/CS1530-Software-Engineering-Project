145a54013afe27047a6ea27810ddd50b990857f8
amm414 <amm414@pitt.edu>
2019-11-11 13:45:59 -0500

    got more stuff to work

--- source/app/routes.py
+++ source/app/routes.py
@@ -14,9 +14,15 @@ if DEBUG:
 # ROUTES START HERE
 #######################################
 
+
+
+
 ### ERROR HANDLING PAGES
-@app.errorhandler(404)
 @app.route('/error')
+def not_found_error_item():
+    return render_template('404.html'), 404
+
+@app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
 
@@ -45,14 +51,14 @@ def login(error=""):
                     CURRENT_USER_ID = i
         if CURRENT_USER_ID > 0:
             return redirect(url_for('user_home_screen'))
-    return render_template('login.html', error=error, page_title=title, css_file=helper_functions.generate_linked_files('login'),)
+    return render_template('login.html', CURRENT_USER_ID=CURRENT_USER_ID, error=error, page_title=title, css_file=helper_functions.generate_linked_files('login'),)
 
 
 # The create account screen
 @app.route('/create-account')
 def create_account():
     title = "Welcome to Craigversity!"
-    return render_template('create-account.html', user_id=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('create-account'), )
+    return render_template('create-account.html',  CURRENT_USER_ID=CURRENT_USER_ID, user_id=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('create-account'), )
 
 
 # The home user logged in screen that lists postings
@@ -62,7 +68,7 @@ def user_home_screen():
         return redirect(url_for('login'))
     title = "Search and Filter Postings!"
     postings = POSTINGS
-    return render_template('user-view.html', able_to_filter=True, user_id=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('user-view'), filtered_postings=postings)
+    return render_template('user-view.html', able_to_filter=True, CURRENT_USER_ID=CURRENT_USER_ID, user_id=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('user-view'), filtered_postings=postings)
 
 
 # The new posting submission screen
@@ -71,7 +77,7 @@ def new_posting_submission():
     if CURRENT_USER_ID == -1:
         return redirect(url_for('login'))
     title = "Submit a New Posting!"
-    return render_template('create-posting-view.html', user_id=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('create-posting-view'))
+    return render_template('create-posting-view.html', user_id=CURRENT_USER_ID,  CURRENT_USER_ID=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('create-posting-view'))
 
 
 # The HELP page
@@ -85,18 +91,15 @@ def help():
 def users_account():
     global CURRENT_USER_ID
     account_info = {}
-    if CURRENT_USER_ID == -1:
-        return redirect(url_for('login'))
-    CURRENT_USER_ID = 3
     if request.method == "GET":
         userid = (request.args.get('userid'))
         for user in USERS:
             if user['userid'] == userid:
                 account_info = user
     if len(account_info) == 0:
-        return redirect(url_for('not_found_error'))
+        return redirect(url_for('not_found_error_item'))
     title = "USER: " + account_info['username']
-    return render_template('account-view.html', user_id=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('account-view'), account=account_info)
+    return render_template('account-view.html', user_id=CURRENT_USER_ID, CURRENT_USER_ID=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('account-view'), account=account_info)
 
 
 # The posting screen
@@ -112,8 +115,4 @@ def full_posting_view():
                 posting_info = post
 
     title = "POSTING: " + posting_info['title']
-    return render_template('full-posting-view.html', user_id=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('full-posting-view'), post=posting_info)
-
-
-if __name__=="__main__":
-    app.run(debug=True)
+    return render_template('full-posting-view.html', user_id=CURRENT_USER_ID, CURRENT_USER_ID=CURRENT_USER_ID, page_title=title, css_file=helper_functions.generate_linked_files('full-posting-view'), post=posting_info)
