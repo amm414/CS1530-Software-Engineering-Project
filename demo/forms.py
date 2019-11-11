3b0b104195e2e0501669e04cc86741bd86a2c147
amm414 <amm414@pitt.edu>
2019-11-11 13:08:16 -0500

    moved to source

--- demo/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
