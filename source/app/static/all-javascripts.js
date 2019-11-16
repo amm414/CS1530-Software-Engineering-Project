9c02f366c6d6cab8867b531edd162cb009bfa24a
amm414 <37627199+amm414@users.noreply.github.com>
2019-11-16 15:14:59 -0500

    merged the changes and added to frontend

---
+++ source/app/static/all-javascripts.js
@@ -0,0 +1,6 @@
+$(document).ready( function(){
+  $("#dropdown-button").click(function(){
+    $("#dropdown-icon").toggleClass("fa-caret-square-down")
+    $("#dropdown-icon").toggleClass("fa-caret-square-up")
+  });
+});
